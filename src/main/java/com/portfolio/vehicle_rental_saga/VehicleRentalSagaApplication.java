4157e9ea16a49b84453d8a8682f974657a00785f
Eduardo Ballesteros Pérez <ballesterospereze@gmail.com>
2026-03-06 11:52:50 +0100

    first commit

---
+++ src/main/java/com/portfolio/vehicle_rental_saga/VehicleRentalSagaApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.vehicle_rental_saga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleRentalSagaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleRentalSagaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/vehicle_rental_saga/VehicleRentalSagaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.vehicle_rental_saga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleRentalSagaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
